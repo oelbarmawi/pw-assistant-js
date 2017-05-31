3d5f14acc87e384f3fc4c9fbb8bb706dd184fa83
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-05-31 10:07:42 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const restService = express();
+
+restService.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+restService.use(bodyParser.json());
+
+restService.post('/webhook', function(req, res) {
+	var oppType = req.body.result.parameters.opp-type;
+	var speech = "This is working -- " + oppType;
+
+	return res.json({
+		speech: speech,
+		displayText: speech,
+		source: 'pw-assistant-test'
+	});
+});
+
+restService.listen((process.env.PORT || 8000), function() {
+	console.log("Server up and running.");
+});
