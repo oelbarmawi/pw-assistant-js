59f5e6ef2ff58b8310e8f802f72e708ef58ea058
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-05-31 11:49:39 -0700

    testing

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/webhook', function(req, res) {
-	parsedBody = JSON.parse(req.body);
 	var speech = "";
+	
 	if (parsedBody.result && parsedBody.result.parameters) {
 		speech = req.body.result.parameters.oppType;
 	} else {
 		speech = "Something went wrong.";
+	}
+	for(var attributename in req) {
+    	speech += attributename + ": " + req[attributename] + "\n";
 	}
     return res.json({
         speech: speech,
