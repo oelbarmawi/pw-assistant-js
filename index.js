e8bf29b2896c9706d64ef805062f80ea90f1ec30
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-05-31 11:30:08 -0700

    Why isn't this workingpt.2

--- index.js
+++ index.js
@@ -5,22 +5,20 @@ const bodyParser = require('body-parser');
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 
 restService.use(bodyParser.json());
 
 restService.post('/webhook', function(req, res) {
-	var speech = req.result.parameters.oppType;
-
-	// var speech = "This is working -- " + oppType;
-	return res.json({
-		speech: speech,
-		displayText: speech,
-		source: 'pw-assistant-test'
-	});
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.oppType ? req.body.result.parameters.oppType : "Seems like some problem. Speak again."
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'pw-assistant-js'
+    });
 });
 
 restService.listen((process.env.PORT || 8000), function() {
-	console.log("Server up and running.");
+    console.log("Server up and running.");
 });
