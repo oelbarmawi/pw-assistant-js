57cb40a413126f958f161e9e2eaad35089963393
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-05-31 12:13:37 -0700

    testing 2

--- index.js
+++ index.js
@@ -26,18 +26,18 @@ restService.post('/webhook', function(req, res) {
 		speech += "4";
 	}
 
-	if (req.body) {
-		speech += "a";
-	}
-	if (req.body.result) {
-		speech += "b";
-	}
-	if (req.body.result.parameters) {
-		speech += "c";
-	} 
-	if (req.body.result.parameters.oppType) {
-		speech += "d";
-	}
+	// if (req.body) {
+	// 	speech += "a";
+	// }
+	// if (req.body.result) {
+	// 	speech += "b";
+	// }
+	// if (req.body.result.parameters) {
+	// 	speech += "c";
+	// } 
+	// if (req.body.result.parameters.oppType) {
+	// 	speech += "d";
+	// }
 
 
 	
