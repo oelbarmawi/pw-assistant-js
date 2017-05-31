64ffa62fae74309b8dd89aa9ab7e0db87256f73c
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-05-31 14:48:50 -0700

    Added switch statements

--- index.js
+++ index.js
@@ -12,12 +12,27 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/webhook', function(req, res) {
-	speech = "Something went wrong, can you repeat the question?";
-	if (req.body.result.action == "TodaysOpportunities.Direct") {
-		var speech = "The following people qualify for ";
-	    speech += req.body.result && req.body.result.parameters && req.body.result.parameters.oppType ? req.body.result.parameters.oppType : "Seems like some problem. Speak again."
-	    speech += ": Bobby, Jonny, Terry, Jerry";
+	var speech = "Something went wrong, can you repeat the question?";
+	var action = req.body.result.action;
+
+	switch(action) {
+		case "TodaysOpportunities.Direct":
+			speech = "The following people qualify for ";
+		    speech += req.body.result && req.body.result.parameters && req.body.result.parameters.oppType ? req.body.result.parameters.oppType : "Seems like some problem. Speak again."
+		    speech += ": Bobby, Jonny, Terry, Jerry"; 
+		    break;
+
+		case "FindClosestPharmacy":
+			var zip = req.body.result.parameters.zipCode;
+			speech = "The closest pharmacy to " + zip " is...";
+			break;
 	}
+
+	// if (req.body.result.action == "TodaysOpportunities.Direct") {
+	// 	speech = "The following people qualify for ";
+	//     speech += req.body.result && req.body.result.parameters && req.body.result.parameters.oppType ? req.body.result.parameters.oppType : "Seems like some problem. Speak again."
+	//     speech += ": Bobby, Jonny, Terry, Jerry";
+	// }
 	return res.json({
         speech: speech,
         displayText: speech,
