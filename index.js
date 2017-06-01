997e087ea152792f77f24b3b06776b044d3e3d21
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-06-01 11:13:05 -0700

    Address functionality

--- index.js
+++ index.js
@@ -22,17 +22,27 @@ restService.post('/webhook', function(req, res) {
 		    speech += ": Bobby, Jonny, Terry, Jerry"; 
 		    break;
 
-		case "FindClosestPharmacy":
-			var zip = req.body.result.parameters.zipCode;
-			speech = "The closest pharmacy to " + zip + " is...";
+		case "FindClosestPharmacy.Address":
+			// var zip = req.body.result.parameters.zipCode;
+			var address = req.body.result.parameters.userAddress;
+			var encodedAddress = encodeURIComponent(userAddress);
+			var ourRequest = new XMLHttpRequest();
+			ourRequest.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&sensor=false');
+			// ourRequest.onload = function() {
+			var place = JSON.parse(ourRequest.responseText);
+			var lat = place.results[0].geometry.location.lat;
+			var lng = place.results[0].geometry.location.lat;
+			var formatted_address = place.results[0].formatted_address;
+			
+			// console.log(formatted_address);
+			// console.log("lat: " + lat + ", lng: " + lng);
+			// };
+			// ourRequest.send();
+			speech = "The latitude is " + lat ", and the longitude is " + lng + " for the address," + formatted_address;
+			// speech = "The closest pharmacy to " + zip + " is...";
 			break;
 	}
 
-	// if (req.body.result.action == "TodaysOpportunities.Direct") {
-	// 	speech = "The following people qualify for ";
-	//     speech += req.body.result && req.body.result.parameters && req.body.result.parameters.oppType ? req.body.result.parameters.oppType : "Seems like some problem. Speak again."
-	//     speech += ": Bobby, Jonny, Terry, Jerry";
-	// }
 	return res.json({
         speech: speech,
         displayText: speech,
