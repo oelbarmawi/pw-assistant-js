e9445181e0f67eb5bbea4f40283465bff1f2a0f1
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-06-02 10:45:06 -0700

    Checking minor errors

--- index.js
+++ index.js
@@ -30,14 +30,13 @@ restService.post('/webhook', function(req, res) {
 			speech = "It's working.";
 
 			// url :: 'https://maps.googleapis.com/maps/api/geocode/json?address=18_kendall_pl&sensor=false'
-			customUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&sensor=false';
-			var res = request('GET', customUrl);
-			var json = JSON.parse(res.getBody());
-			lat = json.results[0].geometry.location.lat;
-			lng = json.results[0].geometry.location.lng;
-			formattedAddress = json.results[0].formatted_address;
-			speech = "The latitude is " + lat + ", and the longitude is " + lng + " for the address " + formattedAddress + ".";
-
+			// customUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&sensor=false';
+			// var res = request('GET', customUrl);
+			// var json = JSON.parse(res.getBody());
+			// lat = json.results[0].geometry.location.lat;
+			// lng = json.results[0].geometry.location.lng;
+			// formattedAddress = json.results[0].formatted_address;
+			// speech = "The latitude is " + lat + ", and the longitude is " + lng + " for the address " + formattedAddress + ".";
 			break;
 	}
 
