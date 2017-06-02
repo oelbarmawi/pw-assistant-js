d3db04395c968476df97f2614b3bcdb2fb4240b7
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-06-02 10:23:46 -0700

    Hopefully this works

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-var request = require('request');
-var request2 = require('sync-request');
+// var request = require('request');
+var request = require('sync-request');
 
 const restService = express();
 
@@ -31,28 +31,13 @@ restService.post('/webhook', function(req, res) {
 
 			// url :: 'https://maps.googleapis.com/maps/api/geocode/json?address=18_kendall_pl&sensor=false'
 			customUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&sensor=false';
-			function getJSON(myurl, callback) {
-				request({url: myurl, method: 'GET', json: true}, function(err, res, json) {
-					if (err) {
-				    	throw err;
-				  	} else {
-				  		lat = json.results[0].geometry.location.lat;
-				  		lng = json.results[0].geometry.location.lng;
-				  		formattedAddress = json.results[0].formatted_address;
-				  		speech = "The latitude is " + lat + ", and the longitude is " + lng + " for the address " + formattedAddress + ".";
-				  		return callback(speech);
-				  	}
-				});
-			}
-			return getJSON(customUrl, returnJSON);
-
-			function returnJSON(text) {
-				return res.json({
-					speech: text,
-					displayText: text,
-					source: 'pw-assistant-js'
-				});
-			}
+			var res = request('GET', customUrl);
+			var json = JSON.parse(res.getBody());
+			lat = json.results[0].geometry.location.lat;
+			lng = json.results[0].geometry.location.lng;
+			formattedAddress = json.results[0].formatted_address;
+			speech = "The latitude is " + lat + ", and the longitude is " + lng + " for the address " + formattedAddress + ".";
+			console.log(speech);
 			break;
 	}
 
