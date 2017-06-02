8cb6cec32368bd47abb1402caf925de851af88cc
Omar Elbarmawi <oelbarmawi@gmail.com>
2017-06-02 10:55:03 -0700

    Not sure if this is working.

--- index.js
+++ index.js
@@ -28,15 +28,16 @@ restService.post('/webhook', function(req, res) {
 			var address = req.body.result.parameters.userAddress;
 			var encodedAddress = encodeURIComponent(address);
 			speech = "It's working.";
+			
 
 			// url :: 'https://maps.googleapis.com/maps/api/geocode/json?address=18_kendall_pl&sensor=false'
-			// customUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&sensor=false';
-			// var res = request('GET', customUrl);
-			// var json = JSON.parse(res.getBody());
-			// lat = json.results[0].geometry.location.lat;
-			// lng = json.results[0].geometry.location.lng;
-			// formattedAddress = json.results[0].formatted_address;
-			// speech = "The latitude is " + lat + ", and the longitude is " + lng + " for the address " + formattedAddress + ".";
+			customUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '&sensor=false';
+			var res = request('GET', customUrl);
+			var json = JSON.parse(res.getBody());
+			lat = json.results[0].geometry.location.lat;
+			lng = json.results[0].geometry.location.lng;
+			formattedAddress = json.results[0].formatted_address;
+			speech = "The latitude is " + lat + ", and the longitude is " + lng + " for the address " + formattedAddress + ".";
 			break;
 	}
 
@@ -49,5 +50,5 @@ restService.post('/webhook', function(req, res) {
 
 
 restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server up and listening");
+    console.log("Server up and listening.");
 });
